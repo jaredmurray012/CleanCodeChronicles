7a4223349f34f7985f78502afed5c3c43bbacade
Jared <Jared@192.168.1.102>
2017-03-14 18:28:42 -0500

    This is part One of LetUsDoMath
    The determineUserSelection() is a little bigger than I would prefer. So
    in the future I will see if I can either trim it down, or add another
    function to make it smaller.

--- LetUsDoMath/src/LetUsDoMathMain.java
+++ LetUsDoMath/src/LetUsDoMathMain.java
@@ -1,12 +1,67 @@
-//Test
-//Test
+// Author Jared Murray 
+// github jaredmurray012 
+// gmail jaredmurray012@gmail.com 
+// Main class file for LetUsDoMath
 
-public class LetUsDoMathMain {
+// IN PROGRESS
 
-	public static void main(String[] args)
+import java.util.InputMismatchException;
+import java.util.Scanner; 
+
+public final class LetUsDoMathMain 
+{
+
+	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		printMathEquationOptions(); 
+		ImplementUserSelection(determineUserSelection()); 
 
+	}
+	
+	public static void printMathEquationOptions()
+	{
+		System.out.println("Please Select the math function below by typing in only the OPTION NUMBER");
+		System.out.println("Option 1: Addition");
+		System.out.println("Option 2: Subtraction");
+	}
+	
+	public static int determineUserSelection() 
+	{
+		int userOptionSelected = 0; 
+		Scanner getOptionFromUser = new Scanner(System.in);
+		
+		try //VALIDATE USER ENTRY 
+		{
+			userOptionSelected = getOptionFromUser.nextInt(); 
+		}
+		
+		catch(InputMismatchException e)
+		{
+			while(userOptionSelected < 0 || userOptionSelected > 9) 
+			{
+				System.out.println("not a valid entry, Please try again, or enter -1 to exit program");
+				userOptionSelected = getOptionFromUser.nextInt();
+				if (userOptionSelected == -1)				
+				{
+					System.exit(0);
+				}
+			}
+			
+			while(e != null) // MEANS that user inputed a character/decimal point 
+			{
+				System.out.println("You did not enter in valid input, program will now shutdown");
+				System.exit(0);
+			}
+		} //END USER VALIDATION 
+		
+		return userOptionSelected; 
+	}
+
+	
+	public static void ImplementUserSelection(int numberFromUserSelection)
+	{
+		System.out.println("Now in ImplementUserSelection");
+		// case statement 
 	}
 
 }
