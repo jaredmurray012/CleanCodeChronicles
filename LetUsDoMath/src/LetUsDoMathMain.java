23d38fe9066d1e1d39f62a17c131fc6a9d0e7eb7
Jared <jaredmurray012@gmail.com>
2017-03-15 20:25:07 -0500

    Made more progress on the program, implementing a case selection block and started to work on the addition functionality.

--- LetUsDoMath/src/LetUsDoMathMain.java
+++ LetUsDoMath/src/LetUsDoMathMain.java
@@ -25,34 +25,40 @@ public final class LetUsDoMathMain
 		System.out.println("Option 2: Subtraction");
 	}
 	
+	
+	// Author's Note: I am not pleased with the size of this function, but I am unaware of the best of way
+	// Of Shortening it. 
 	public static int determineUserSelection() 
 	{
-		int userOptionSelected = 0; 
 		Scanner getOptionFromUser = new Scanner(System.in);
+		
+		int userOptionSelected = 0; 
 		
 		try //VALIDATE USER ENTRY 
 		{
-			userOptionSelected = getOptionFromUser.nextInt(); 
-		}
-		
-		catch(InputMismatchException e)
-		{
-			while(userOptionSelected < 0 || userOptionSelected > 9) 
+			userOptionSelected = getOptionFromUser.nextInt();
+			
+			while(userOptionSelected < 0 || userOptionSelected > 5) 
 			{
 				System.out.println("not a valid entry, Please try again, or enter -1 to exit program");
 				userOptionSelected = getOptionFromUser.nextInt();
+				
 				if (userOptionSelected == -1)				
 				{
 					System.exit(0);
 				}
 			}
-			
+		}
+		
+		catch(InputMismatchException e)
+		{	
 			while(e != null) // MEANS that user inputed a character/decimal point 
 			{
 				System.out.println("You did not enter in valid input, program will now shutdown");
 				System.exit(0);
 			}
 		} //END USER VALIDATION 
+		
 		
 		return userOptionSelected; 
 	}
@@ -61,7 +67,63 @@ public final class LetUsDoMathMain
 	public static void ImplementUserSelection(int numberFromUserSelection)
 	{
 		System.out.println("Now in ImplementUserSelection");
-		// case statement 
+		
+		switch(numberFromUserSelection)
+		{
+		case 1: 
+			additionEquation();
+			break; 
+			
+		case 2:
+			System.out.println("Math Option Two here");
+			break;
+			
+		case 3:
+			System.out.println("Math Option Three here");
+			break;
+			
+		case 4:
+			System.out.println("Math Option Four here");
+			break;
+			
+		default: 
+			System.out.println("Invalid option selected, Program will now close"); 
+			System.exit(0);
+			
+		}
+		 Finally: 
+			 System.out.println("A fine selection, one moment please.");
+		
+	}
+	
+	public static int additionEquation()
+	{
+		firstOperand(); 
+		secondOperand(); 
+		
+		return firstOperand() + secondOperand(); 
+	}
+	
+	// Purpose is to ask user for input and validate said input and return it. 
+	public static int firstOperand()
+	{
+		Scanner OperandFromUser = new Scanner(System.in);
+		
+		int userSelectedOperand = 0;
+		
+		System.out.println("Please enter a positive whole number"); 
+		
+		// LEFT OFF AT THE USER VALIDATION FOR THIS FUNCTION
+		
+		return 0; 
+		
+		
+	}
+	
+	// Purpose is to ask user for input and validate said input and return it. 
+	public static int secondOperand()
+	{
+		return 0; 
 	}
 
 }
