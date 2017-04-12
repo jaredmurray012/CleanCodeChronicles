05bf8ddbd58b6968e21b35f27924d49089202d07
Jared <jaredmurray012@gmail.com>
2017-04-12 13:25:58 -0500

    Added addition, subtraction, division, and multiply methods in the Math Equations class.
    Also added additional print statements, started more work in the main
    class.

--- LetUsDoMath/src/LetUsDoMathMain.java
+++ LetUsDoMath/src/LetUsDoMathMain.java
@@ -6,15 +6,34 @@
 // IN PROGRESS... 
 
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class LetUsDoMathMain 
 {
 
 	public static void main(String[] args) 
 	{
+		String again = ""; 
+		Scanner input = new Scanner(System.in); 
 		mathEquations test = new mathEquations(); 
 		
-		test.askForInput();
+		test.intialPrintStatements();
+		
+		do 
+		{
+			test.menuOptionsExplanation();
+			
+			
+			
+		}while(again.startsWith("y")|| again.startsWith("Y"));
+		
+		input.close();
+		//test.askForInput();
+		//test.addition(); 
+		//test.subtraction();
+		//test.multiplication();
+		//test.division();
+		
 		
 		
 
